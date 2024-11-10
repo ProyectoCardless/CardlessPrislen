9b5a6610f6ab2d5597385d51733e87edd4cd100a
ProyectoCardless <proyectocardless@gmail.com>
2024-11-10 10:46:18 -0600

    Update JsonLogObserver.java

--- src/main/java/com/banco/CajerosCardless/observers/JsonLogObserver.java
+++ src/main/java/com/banco/CajerosCardless/observers/JsonLogObserver.java
@@ -9,18 +9,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class JsonLogObserver implements LogObserver {
+    private static final String TEMP_DIR = System.getProperty("java.io.tmpdir") + "/log.json";
+    private static final String LOCAL_DIR = "src/main/resources/static/logs/log.json";
 
     private final ObjectMapper objectMapper;
 
-    // Constructor to receive the configured ObjectMapper
     public JsonLogObserver(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
     @Override
     public void logAction(LogEntry logEntry) {
-        try (FileWriter writer = new FileWriter("logs/log.json", true)) {
-            writer.write(objectMapper.writeValueAsString(logEntry) + "\n");
+        try {
+            String logData = objectMapper.writeValueAsString(logEntry) + "\n";
+            writeToFile(TEMP_DIR, logData);
+            writeToFile(LOCAL_DIR, logData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToFile(String filePath, String logData) {
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            writer.write(logData);
         } catch (IOException e) {
             e.printStackTrace();
         }
