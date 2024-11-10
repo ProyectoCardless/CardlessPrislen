968a4a18d7c0881fa2daf0c88bc95b5e25b74457
ProyectoCardless <proyectocardless@gmail.com>
2024-11-10 10:46:38 -0600

    Update PositionalLogObserver.java

--- src/main/java/com/banco/CajerosCardless/observers/PositionalLogObserver.java
+++ src/main/java/com/banco/CajerosCardless/observers/PositionalLogObserver.java
@@ -9,16 +9,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class PositionalLogObserver implements LogObserver {
+    private static final String TEMP_DIR = System.getProperty("java.io.tmpdir") + "/log.pos";
+    private static final String LOCAL_DIR = "src/main/resources/static/logs/log.pos";
 
     @Override
     public void logAction(LogEntry logEntry) {
-        try (FileWriter writer = new FileWriter("logs/log.pos", true)) {
-            writer.write(String.format("%-20s %-15s %-20s %-15s %-20s\n",
-                    logEntry.getTimestamp(),
-                    logEntry.getIpAddress(),
-                    logEntry.getOperatingSystem(),
-                    logEntry.getCountry(),
-                    logEntry.getAction()));
+        String logData = String.format("%-20s %-15s %-20s %-15s %-20s\n",
+                logEntry.getTimestamp(),
+                logEntry.getIpAddress(),
+                logEntry.getOperatingSystem(),
+                logEntry.getCountry(),
+                logEntry.getAction());
+
+        // Escribe el log en ambos archivos
+        writeToFile(TEMP_DIR, logData);
+        writeToFile(LOCAL_DIR, logData);
+    }
+
+    private void writeToFile(String filePath, String logData) {
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            writer.write(logData);
         } catch (IOException e) {
             e.printStackTrace();
         }
